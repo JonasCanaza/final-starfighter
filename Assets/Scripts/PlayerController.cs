fd20e17427bbe524242e6a2223bf5d4ddf2ad1f1
Jonas <jonascanaza@gmail.com>
2026-04-26 19:28:01 -0300

    Add player movement limit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement Setting")]
-    [SerializeField] float speed = 15.0f;
+    [SerializeField] private float speed = 15.0f;
     private float moveInput;
+
+    [Header("References")]
+    [SerializeField] private SpriteRenderer visual;
+
+    private Camera mainCamera;
+    private float leftScreenLimit;
+    private float rightScreenLimit;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+
+        float screenLeft = mainCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x;
+        float screenRight = mainCamera.ViewportToWorldPoint(new Vector3(1f, 0f, 0f)).x;
+        float halfPlayerWidth = visual.bounds.extents.x;
+
+        leftScreenLimit = screenLeft + halfPlayerWidth;
+        rightScreenLimit = screenRight - halfPlayerWidth;
+    }
 
     private void Update()
     {
@@ -21,6 +40,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 newPosition = transform.position;
         newPosition.x += moveInput * speed * Time.deltaTime;
+        newPosition.x = Mathf.Clamp(newPosition.x, leftScreenLimit, rightScreenLimit);
         transform.position = newPosition;
     }
 }
