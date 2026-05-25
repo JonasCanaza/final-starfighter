1e21a94fa7d99640504bfa01447dd54f758a1fa1
Jonas <jonascanaza@gmail.com>
2026-05-25 19:12:19 -0300

    Add AudioManager and MonoBehaviourSingleton classes

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviourSingleton<AudioManager>
+{
+    public bool IsMusicPaused { get; private set; }
+
+    [Header("Sources Settings")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    [Header("Clips Settings")]
+    [SerializeField] private AudioClip gameplayMusic;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        if (musicSource.clip != clip)
+        {
+            musicSource.clip = clip;
+        }
+
+        musicSource.enabled = true;
+        musicSource.loop = true;
+
+        if (!musicSource.isPlaying)
+        {
+            musicSource.Play();
+        }
+
+        IsMusicPaused = false;
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+        IsMusicPaused = false;
+    }
+
+    public void PauseMusic()
+    {
+        if (!IsMusicPaused)
+        {
+            musicSource.Pause();
+            IsMusicPaused = true;
+        }
+    }
+
+    public void ResumeMusic()
+    {
+        if (IsMusicPaused)
+        {
+            musicSource.UnPause();
+            IsMusicPaused = false;
+        }
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        PlayMusic(gameplayMusic);
+    }
+}
