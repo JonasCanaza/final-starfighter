3de0e0d59b9e8e9665c5765753c206cef485d5bc
Jonas <jonascanaza@gmail.com>
2026-06-05 22:45:58 -0300

    Add exit game button to main menu

--- Assets/Scripts/UI/Main Menu/MainPanelController.cs
+++ Assets/Scripts/UI/Main Menu/MainPanelController.cs
@@ -6,19 +6,31 @@ public class MainPanelController : MonoBehaviour
 {
     [Header("Button References")]
     [SerializeField] private Button playButton;
+    [SerializeField] private Button exitButton;
 
     private void Awake()
     {
         playButton.onClick.AddListener(HandlePlayClicked);
+        exitButton.onClick.AddListener(HandleExitClicked);
     }
 
     private void OnDestroy()
     {
         playButton.onClick.RemoveListener(HandlePlayClicked);
+        exitButton.onClick.RemoveListener(HandleExitClicked);
     }
 
     private void HandlePlayClicked()
     {
         SceneManager.LoadScene(SceneNames.Gameplay);
+    }
+
+    private void HandleExitClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
